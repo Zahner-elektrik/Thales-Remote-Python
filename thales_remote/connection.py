034414f880844ed652255bda1ea6224f08696e4c
Ilka Schulz <schulz@ifes.uni-hannover.de>
2023-01-11 14:12:32 +0100

    add type annotations and some formatting to thales_remote.{connection,error,file_interface} and half way through script_wrapper
    also:
    - format code with "black"
    - rename variables according to PEP8
    - write comments which are not documentation as comments, not as docstrings
    - replace "foo == None" with "foo is None"
    - replace "if foo == True:" with "if foo:"
    - sort import statements
    - remove unused import statements
    - remove underscore from "from _queue import Empty"
    - replaced obscured calls to "return None" with literal statement, e.g.
    
      retval = None
      if some_condition:
    -   return retval
    +   return None
    
    - added some TODOs
    - fix typos in private attribute and method names (not in public interface)
    - bugfix in ThalesFileInterface.appendFilesToSkip: properly handle both `str` and `list[str]`
    - use context manager for file operations
    - replace calls to `"foo".find("bar") with `"bar" in "foo"`
    - slightly improve docstrings
    - use dict in ThalesRemoteScriptWrapper.setPotentiostatMode instead of elif-statements

--- thales_remote/connection.py
+++ thales_remote/connection.py
@@ -29,6 +29,7 @@ import time
 import struct
 import threading
 import queue
+from typing import Optional, Union, Any
 from _socket import SHUT_RD
 from thales_remote.error import TermConnectionError
 
@@ -39,142 +40,158 @@ class ThalesRemoteConnection(object):
     """
     Class to handle the Thales remote connection.
     """
-    
+
+    _term_port: int
+    _socket_handle: Optional[socket.socket]
+    _receiving_worker: Optional[threading.Thread]
+    _send_mutex: threading.Semaphore
+    _receiving_worker_is_running: bool
+    _available_channels: list[int]
+    _queuesForChannels: dict[int, queue.Queue[Optional[bytes]]]
+    _connectionName: str
+
     def __init__(self):
-        self.term_port = 260  # The port used by Thales
-        self.socket_handle = None
-        
-        self.receivingWorker = None
-        
-        self.sendMutex = threading.Semaphore(1)
-        
+        self._term_port = 260  # The port used by Thales
+        self._socket_handle = None
+        self._receiving_worker = None
+        self._send_mutex = threading.Semaphore(1)
         self._receiving_worker_is_running = False
-        
-        self.availibleChannels = [2,128,129,130,131,132]
-        
-        self.queuesForChannels = dict()
-        
-        for channel in self.availibleChannels:
-            self.queuesForChannels[channel] = queue.Queue()
-        
-        self.connectionName = ""
-        return
-        
-    def connectToTerm(self, address, connectionName = "ScriptRemote"):
-        """ Connect to Term Software.
-        
-        
-        :param address: The hostname or ip-address of the host running Term.
-        :param connectionName: The name of the connection ScriptRemote for Remote and Logging as Online Display.
-        :returns: True on success, False if failed.
-        :rtype: bool
+        self._available_channels = [2, 128, 129, 130, 131, 132]
+        self._queuesForChannels = dict()
+
+        for channel in self._available_channels:
+            self._queuesForChannels[channel] = queue.Queue()
+
+        self._connectionName = ""
+
+    def connectToTerm(
+        self, address: str, connection_name: str = "ScriptRemote"
+    ) -> bool:
         """
-        self.socket_handle = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        Connect to Term Software.
+
+        TODO:
+            - should raise exception on failure – returning bools is C-style error handling
+
+        :param address: hostname or ip-address of the host running "Term" application
+        :param connection_name: name of the connection ScriptRemote for Remote and Logging as Online Display
+        :returns: True on success, False on failure
+        """
+        self._socket_handle = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            self.socket_handle.connect((address, self.term_port))
+            self._socket_handle.connect((address, self._term_port))
         except:
-            self.socket_handle = None
+            self._socket_handle = None
             return False
-            
+
         self._startTelegramListener()
-        
+
         time.sleep(0.4)
-        
-        self.connectionName = connectionName
-        payload_length = len(connectionName)
-        
+
+        self._connectionName = connection_name
+        payload_length = len(connection_name)
+
         registration_packet = bytearray()
-        registration_packet += bytearray(struct.pack('<H', payload_length))
-        registration_packet += bytearray([0x02, 0xd0, 0xff, 0xff, 0xff, 0xff])
-        registration_packet += bytearray(connectionName, 'ASCII')
-        
+        registration_packet += bytearray(struct.pack("<H", payload_length))
+        registration_packet += bytearray([0x02, 0xD0, 0xFF, 0xFF, 0xFF, 0xFF])
+        registration_packet += bytearray(connection_name, "ASCII")
+
         # print("\n" + str(datetime.now().time()) + " send:")
         # print(f"payload_length: {payload_length}")
         # print(f"registration_packet: {registration_packet}")
         # print("complete packet:" + str(registration_packet))
-        
-        self.sendMutex.acquire()
-        self.socket_handle.sendall(registration_packet)
-        self.sendMutex.release()
-        
+
+        self._send_mutex.acquire()
+        self._socket_handle.sendall(registration_packet)
+        self._send_mutex.release()
+
         time.sleep(0.8)
-        
+
         return True
-    
-    def getConnectionName(self):
-        """ Get the connection name
-        
-        :returns: The name of the connection.
-        :rtype: str
+
+    def getConnectionName(self) -> str:
         """
-        return self.connectionName
-            
-    def disconnectFromTerm(self):
-        """ Close the connection to Term and cleanup.
-        
+        get the connection name
+
+        :returns: name of the connection
+        """
+        return self._connectionName
+
+    def disconnectFromTerm(self) -> None:
+        """
+        close the connection to Term and cleanup
+
         Stops the thread used for receiving telegrams assynchronously and shuts down
         the network connection. Put None into the Queues to free the waiting threads.
         They wait in waitForBinaryTelegram and if they receive None from the Queue, the will throw an exception.
         """
-        self.sendStringAndWaitForReplyString("3," + str(self.connectionName) + ",0,RS", 128)
+        self.sendStringAndWaitForReplyString(
+            "3," + str(self._connectionName) + ",0,RS", 128
+        )
         time.sleep(0.2)
-        self.sendTelegram(bytearray([255,255]), 4)
+        self.sendTelegram(bytearray([255, 255]), 4)
         time.sleep(0.2)
         self._stopTelegramListener()
         time.sleep(0.2)
         self._closeSocket()
-        for key in self.queuesForChannels.keys():
-            self.queuesForChannels[key].put(None)
+        for key in self._queuesForChannels.keys():
+            self._queuesForChannels[key].put(None)
         return
-        
-    def isConnectedToTerm(self):
-        """Check if the connection to Term is open.
-        
-        :returns: True if connected, False if not.
-        :rtype: bool
+
+    def isConnectedToTerm(self) -> bool:
         """
-        return self.socket_handle != None
-            
-    def sendTelegram(self, payload, message_type, timeout=None):
-        """Send a telegram (data) to Term.
-        
+        check if the connection to Term is open
+
+        :returns: True if connected, False otherwise
+        """
+        return self._socket_handle != None
+
+    def sendTelegram(
+        self,
+        payload: Union[str, bytearray],
+        message_type: int,
+        timeout: Optional[float] = None,
+    ) -> None:
+        """
+        send a telegram (data) to Term
+
         Sending a telegram to the term.
         If the other thread hangs while sending and the semaphore cannot be aquired within the timout,
         then a TermConnectionError is thrown. If a timeout occurs in the socket,
         then an exception is thrown by the socket.
-        
+
         :param payload: The actual data which is being sent to Term. This can be a string or an bytearray.
         :param message_type: Used internally by the DevCli dll. Depends on context. Most of the time 2.
         :param timeout: The timeout for sending data in seconds, blocking at None.
         """
         packet = bytearray()
         data = bytearray()
-        
-        if(isinstance(payload, str)):
+
+        if isinstance(payload, str):
             # datatype string
             payload_length = len(payload)
-            data += bytearray(payload, 'ASCII')
+            data += bytearray(payload, "ASCII")
         else:
             # datatype bytearray
             payload_length = len(payload)
             data = payload
-        
-        packet += bytearray(struct.pack('<H', payload_length))
-        packet += bytearray(struct.pack('<B', message_type))
+
+        packet += bytearray(struct.pack("<H", payload_length))
+        packet += bytearray(struct.pack("<B", message_type))
         packet += data
-        
-        if self.sendMutex.acquire(True,timeout=timeout):
-            self.socket_handle.settimeout(timeout)
+
+        if self._send_mutex.acquire(True, timeout=timeout):
+            self._socket_handle.settimeout(timeout)
             try:
                 # print("\n" + str(datetime.now().time()) + " send:")
                 # print(f"payload_length: {payload_length} message_type: {message_type}")
                 # print(f"payload: {data}")
                 # print("complete packet:" + str(packet))
-                
-                self.socket_handle.sendall(packet)
+
+                self._socket_handle.sendall(packet)
             finally:
-                self.socket_handle.settimeout(None)
-                self.sendMutex.release()
+                self._socket_handle.settimeout(None)
+                self._send_mutex.release()
         else:
             """
             The semaphore to send could not be aquired within the timeout.
@@ -182,119 +199,135 @@ class ThalesRemoteConnection(object):
             """
             raise TermConnectionError("Socket error during data transmission.")
         return
-    
-    
-    def waitForBinaryTelegram(self, message_type=2, timeout=None):
-        """ Block infinitely until the next Telegram is arriving.
-        
+
+    def waitForBinaryTelegram(
+        self, message_type: int = 2, timeout: Optional[float] = None
+    ) -> bytes:
+        """
+        block infinitely until the next Telegram is arriving
+
         If some Telegram has already arrived it will just return the last one from the queue.
-        
+
         :param message_type: Used internally by the DevCli dll. Depends on context. Most of the time 2.
         :param timeout: The timeout for sending data in seconds, blocking at None
         :returns: The response from the device or an empty bytearray if someting went wrong.
         :rtype: bytearray
         """
-        retval = self.queuesForChannels[message_type].get(True, timeout=timeout)
-        if retval == None:
+        retval = self._queuesForChannels[message_type].get(True, timeout=timeout)
+        if retval is None:
             raise TermConnectionError("Socket error during data reception.")
         return retval
-            
-    def waitForStringTelegram(self, message_type=2, timeout=None):
-        """ Block infinitely until the next Telegram is arriving.
-        
+
+    def waitForStringTelegram(
+        self, message_type: int = 2, timeout: Optional[float] = None
+    ) -> str:
+        """
+        block infinitely until the next Telegram is arriving
+
         If some Telegram has already arrived it will just return the last one from the queue.
-        
+
         :param message_type: Used internally by the DevCli dll. Depends on context. Most of the time 2.
         :param timeout: The timeout for sending data in seconds, blocking at None
         :returns: The last received telegram or an empty string if someting went wrong.
         :rtype: string
         """
-        retval = self.waitForBinaryTelegram(message_type,timeout).decode("ASCII")
+        retval = self.waitForBinaryTelegram(message_type, timeout).decode("ASCII")
         return retval
-        
-    def sendStringAndWaitForReplyString(self, payload, message_type, timeout=None, answer_message_type = None):
-        """ Convenience function: Send a telegram and wait for it's reply.
-        
+
+    def sendStringAndWaitForReplyString(
+        self,
+        payload: Union[str, bytearray],
+        message_type: int,
+        timeout: Optional[float] = None,
+        answer_message_type: int = None,
+    ) -> str:
+        """
+        convenience function: send a telegram and wait for it's reply
+
         If a timeout or a socket error occurs an exception is thrown.
-        
-        
+
+
         :param payload: The actual data which is being sent to Term. This can be a string or an bytearray.
         :param message_type: Used internally by the DevCli dll. Depends on context. Most of the time 2.
         :param timeout: The timeout for sending data in seconds, blocking at None.
         :returns: The last received telegram or an empty string if someting went wrong.
         :rtype: string
         """
-        if answer_message_type == None:
+        if answer_message_type is None:
             answer_message_type = message_type
         self.sendTelegram(payload, message_type, timeout)
         return self.waitForStringTelegram(answer_message_type, timeout)
-        
-    """
-    The following methods should not be called by the user.
-    They are marked with the prefix '_' after the Python convention for proteced.
-    """
-        
-    def _telegramListenerJob(self):
-        """ The method running in a separate thread, pushing the incomming packets into the queues.
+
+    # The following methods should not be called by the user.
+    # They are marked with the prefix '_' after the Python convention for proteced.
+
+    def _telegramListenerJob(self) -> None:
+        """
+        runs in a separate thread, pushing the incomming packets into the queues.
         """
         while self._receiving_worker_is_running:
             message_type, telegram = self._readTelegramFromSocket()
-            if len(telegram) > 0 and message_type in self.availibleChannels:
-                self.queuesForChannels[message_type].put(telegram)
-            elif message_type == None:
-                """
-                An error has occurred in the connection. None is passed into all queues to free
-                the waiting threads from the queue. If they have received None, they throw an exception.
-                The thread is then exited.
-                """
-                for key in self.queuesForChannels.keys():
-                    self.queuesForChannels[key].put(None)
+            if len(telegram) > 0 and message_type in self._available_channels:
+                self._queuesForChannels[message_type].put(telegram)
+            elif message_type is None:
+                # An error has occurred in the connection. None is passed into all queues to free
+                # the waiting threads from the queue. If they have received None, they throw an exception.
+                # The thread is then exited.
+                for key, value in self._queuesForChannels.items():
+                    value.put(None)
                 self._receiving_worker_is_running = False
         return
-            
-    def _startTelegramListener(self):
-        """ Starts the thread handling the asyncronously incoming data.
+
+    def _startTelegramListener(self) -> None:
+        """
+        starts the thread handling the asyncronously incoming data
         """
         self._receiving_worker_is_running = True
-        self.receivingWorker = threading.Thread(target=self._telegramListenerJob)
-        self.receivingWorker.start()
+        self._receiving_worker = threading.Thread(target=self._telegramListenerJob)
+        self._receiving_worker.start()
         return
-        
-    def _stopTelegramListener(self):
-        """ Stops the thread handling the incoming data gracefully.
+
+    def _stopTelegramListener(self) -> None:
         """
-        self.socket_handle.shutdown(SHUT_RD)
+        stops the thread handling the incoming data gracefully
+        """
+        self._socket_handle.shutdown(SHUT_RD)
         self._receiving_worker_is_running = False
-        self.receivingWorker.join()
+        self._receiving_worker.join()
         return
-            
-    def _readTelegramFromSocket(self):
-        """ Reads the raw telegram structure from the socket stream.
-        
+
+    def _readTelegramFromSocket(self) -> tuple[Optional[str], bytearray]:
+        """
+        reads the raw telegram structure from the socket stream
+
         When a socket exception occurs, None and an empty byte array are returned.
         The caller of the function then passes the None to the queue to raise an
         exception in the threads waiting at the queue.
         """
         try:
-            header_len = self.socket_handle.recv(2)
-            header_type_bytes = self.socket_handle.recv(1)
-            header_type = struct.unpack('<B', header_type_bytes)[0]
-            incoming_packet = self.socket_handle.recv(struct.unpack('<H', header_len)[0])
-            
+            header_len: bytes = self._socket_handle.recv(2)
+            header_type_bytes: bytes = self._socket_handle.recv(1)
+            header_type: str = struct.unpack("<B", header_type_bytes)[
+                0
+            ]  # actually a character, not a str
+            incoming_packet: int = self._socket_handle.recv(
+                struct.unpack("<H", header_len)[0]
+            )
+
             # print("\n" + str(datetime.now().time()) + " read:")
             # print(f"payload_length: {struct.unpack('<H', header_len)[0]} message_type: {header_type}")
             # print(f"payload: {incoming_packet}")
             # print("complete packet:" + str(header_len + header_type_bytes + incoming_packet))
-            
+
         except:
             header_type = None
             incoming_packet = bytearray()
         return header_type, incoming_packet
-            
-    def _closeSocket(self):
-        """ Close the socket.
+
+    def _closeSocket(self) -> None:
         """
-        self.socket_handle.close()
-        self.socket_handle = None
+        close the socket
+        """
+        self._socket_handle.close()
+        self._socket_handle = None
         return
-        
